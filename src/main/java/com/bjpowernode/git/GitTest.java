c815add1ef4279e115e378782654ec44535f861c
leng <leng@614322108.com>
2021-06-15 12:46:08 +0800

    push commit

--- src/main/java/com/bjpowernode/git/GitTest.java
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hot-fix hello git 4");
         System.out.println("master commit");
         System.out.println("hot-fix hello g");
+        System.out.println("push test");
     }
 }
